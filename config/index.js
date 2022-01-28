355e74b1af6d59fc22c4c3eaa054c643039e0982
Astrospence <illustrator.wood@gmail.com>
2022-01-27 17:15:52 -0700

    completed restricted.js-added config file for jwt

---
+++ config/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    BCRYPT_ROUNDS: process.env.BCRYPT_ROUNDS || 3,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    PORT: process.env.PORT || 9000,
+    JWT_SECRET: process.env.JWT_SECRET || 'I wish Groudon were a lengendary creature card in magic the gathering',
+  }
