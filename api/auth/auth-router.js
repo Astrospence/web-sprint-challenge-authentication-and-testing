19b8c615a5e9dcd32d5307f1c6a89ea34fa54b27
Astrospence <illustrator.wood@gmail.com>
2022-02-01 09:10:02 -0700

    cleanup

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -10,7 +10,6 @@ router.post('/register', checkReqBody, checkUsernameExists, (req, res, next) => 
   let user = req.body
   const hash = bcrypt.hashSync(user.password, BCRYPT_ROUNDS || 3)
   user.password = hash
-  console.log(user)
 
   Db.insert(user)
     .then(response => {
